c8f426aeb1a28a6c284bc61b1d33a5e55796f09c
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-04-18 18:46:12 +0530

    Added an infinite loop for syncing pods.

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"strings"
+	"time"
 
 	"zerok.ai/deamonset/detector"
-	server "zerok.ai/deamonset/server"
 	types "zerok.ai/deamonset/types"
 	zkclient "zerok.ai/deamonset/zkclient"
 )
@@ -15,21 +13,9 @@ func main() {
 	injectorClient := &zkclient.InjectorClient{
 		ContainerResults: []types.ContainerRuntime{},
 	}
-	fmt.Println("Start lang detection.")
-	result := parseArgs()
-	fmt.Println("The args are ", result)
-	detector.ReScanPods(injectorClient)
-	server.StartServer()
-}
-
-func parseArgs() *server.LangDetect {
-	result := server.LangDetect{}
-	var names string
-	flag.StringVar(&result.PodUID, "pod-uid", "", "The UID of the target pod")
-	flag.StringVar(&names, "container-names", "", "The container names in the target pod")
-	flag.Parse()
-
-	result.Containers = strings.Split(names, ",")
-
-	return &result
+	for {
+		fmt.Println("Rescaning pods")
+		detector.ReScanPods(injectorClient)
+		time.Sleep(5 * time.Second)
+	}
 }
