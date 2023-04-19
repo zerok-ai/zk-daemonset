02a9f5d385aa669c57c7a847e9e6999ef159db39
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-04-19 15:46:17 +0530

    Minor changes

--- common/const.go
+++ common/const.go
@@ -3,4 +3,3 @@ package common
 const ZkOrchStatusKey = "zk-status"
 const ZkOrchStatusPath = "/metadata/labels/" + ZkOrchStatusKey
 const ZkOrchScanned = "scanned"
-const ZkOrchOrchestrated = "orchestrated"
