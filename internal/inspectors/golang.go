551f108ab60246472a5153cc570e5e8d43ca16f8
Shivam Nagar <shivam.nagar@outlook.com>
2023-06-27 14:57:27 +0530

    Added golang inspector

---
+++ internal/inspectors/golang.go
@@ -0,0 +1,37 @@
+package inspectors
+
+import (
+	"fmt"
+	"os"
+	"zk-daemonset/internal/inspectors/goversion"
+	types "zk-daemonset/internal/models"
+)
+
+type golangInspector struct{}
+
+var golang = &golangInspector{}
+
+const golangProcessName = "go"
+
+func (g *golangInspector) Inspect(p *types.ProcessDetails) (types.ProgrammingLanguage, bool) {
+	file := fmt.Sprintf("/proc/%d/exe", p.ProcessID)
+	_, err := os.Stat(file)
+	if err != nil {
+		fmt.Printf("could not perform os.stat: %s\n", err)
+		return "", false
+	}
+
+	x, err := goversion.OpenExe(file)
+	if err != nil {
+		fmt.Printf("could not perform OpenExe: %s\n", err)
+		return "", false
+	}
+
+	vers, _ := goversion.FindVersion(x)
+	if vers == "" {
+		// Not a golang app
+		return "", false
+	}
+
+	return types.GoProgrammingLanguage, true
+}
